Fix issue with rotation

cube.py
@@ -120,9 +120,9 @@ class Cube:
     
     def setCube(self,rotationMatrix):
         up = np.delete(rotationMatrix[0],[0,-1])
-        down = np.delete(rotationMatrix[2],[0,-1])
+        down = np.delete(rotationMatrix[4],[0,-1])
         left = np.delete(rotationMatrix[:,0],[0,-1])
-        right = np.delete(rotationMatrix[:,2],[0,-1])
+        right = np.delete(rotationMatrix[:,4],[0,-1])
         front = rotationMatrix[1:-1,1:-1]
         return front,up,down,right,left
     

main.py
@@ -3,6 +3,8 @@ import numpy as np
 import nerd
 
 cube1 = cube.Cube()
-cube1.d[0] = np.array(["messing","a","bout"])
 
-print(nerd.Nerd.whiteCrossComplete(cube1))
+cube1.rotateSide("l")
+
+print(cube1.f)
+print(cube1.l)